9f63e97ca2e073c723e40142d10bf17edc29bd23
slrslrr2 <slrslrr1@gmail.com>
2022-03-08 00:42:30 +0900

    소스 정리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,18 +13,20 @@ public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    // @Autowired
-    // 생성자 함수가 하나인 경우 생략 가능
-    // @RequiredArgsConstructor
-    // ㄴ 해당 메소드를 사용하면 필수 파라미터인 final들을 파라미터로 생성자함수를 만들어준다.
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
-    // 결과적으로, @Autowired를 생략하고
-    // @RequiredArgsConstructor를 적용하면서 final생성함수를 자동으로 생성자를 만들어준다
-    // 롬복 짱짱짱
-    
+    /**
+    @Autowired
+    생성자 함수가 하나인 경우 생략 가능
+    @RequiredArgsConstructor
+    ㄴ 해당 메소드를 사용하면 필수 파라미터인 final들을 파라미터로 생성자함수를 만들어준다.
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+    결과적으로, @Autowired를 생략하고
+    @RequiredArgsConstructor를 적용하면서 final생성함수를 자동으로 생성자를 만들어준다
+    롬복 짱짱짱
+    **/
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -2,15 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class RateDiscountPolicyTest {
     DiscountPolicy discountPolicy = new RateDiscountPolicy();
