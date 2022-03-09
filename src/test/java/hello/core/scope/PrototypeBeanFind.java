d23446fce45c2a2ea32da279eb1d709121ad7e8a
slrslrr2 <slrslrr1@gmail.com>
2022-03-10 00:06:32 +0900

    빈 스코프 - 프로토타입 스코프

---
+++ src/test/java/hello/core/scope/PrototypeBeanFind.java
@@ -0,0 +1,59 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeBeanFind {
+
+    @Test
+    public void singletonBeanFine(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ProtoTypeBean.class);
+        ProtoTypeBean protoTypeBean = ac.getBean(ProtoTypeBean.class);
+        ProtoTypeBean protoTypeBean2 = ac.getBean(ProtoTypeBean.class);
+
+        System.out.println("find prototypeBean");
+        System.out.println("protoTypeBean = " + protoTypeBean);
+        System.out.println("find prototypeBean2");
+        System.out.println("protoTypeBean2 = " + protoTypeBean2);
+
+        assertThat(protoTypeBean).isNotSameAs(protoTypeBean2);
+        ac.close();
+
+        /**
+         * ProtoTypeBean.init
+         * ProtoTypeBean.init
+         *
+         * find prototypeBean
+         * protoTypeBean = hello.core.scope.PrototypeBeanFind$ProtoTypeBean@78aea4b9
+         *
+         * find prototypeBean2
+         * protoTypeBean2 = hello.core.scope.PrototypeBeanFind$ProtoTypeBean@2a65bb85
+         */
+
+        /**
+         * 초기화 메서드 실행 시점 : 빈 조회할 때 생성
+         * 프로토타입 빈을 2번 조회하고, *다른 스코프 빈이 생성*됨, 초기화도 2번 실행
+         * 스프링컨테이너가 생성과 의존관계주입만 관여함
+         * 스프링컨테이너가 종료는 관리 안하기에 destroy 실행안함
+         */
+    }
+
+    @Scope("prototype")
+    static class ProtoTypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("ProtoTypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("ProtoTypeBean.destroy");
+        }
+    }
+}
---
+++ src/test/java/hello/core/scope/SingletonBeanFind.java
@@ -0,0 +1,55 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonBeanFind {
+
+    @Test
+    public void singletonBeanFine(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println("singletonBean = " + singletonBean);
+        System.out.println("singletonBean2 = " + singletonBean2);
+
+        assertThat(singletonBean).isSameAs(singletonBean2);
+        ac.close();
+
+        /**
+         * SingletonBean.init
+         * singletonBean = hello.core.scope.SingletonBeanFind$SingletonBean@78aea4b9
+         * singletonBean2 = hello.core.scope.SingletonBeanFind$SingletonBean@78aea4b9
+         * 23:57:23.164 [Test worker] DEBUG org.springframework.context.annotation.AnnotationConfigApplicationContext - Closing org.springframework.context.annotation.AnnotationConfigApplicationContext@7e3181aa, started on Wed Mar 09 23:57:22 KST 2022
+         * SingletonBean.destroy
+         */
+
+        /**
+         * @Scope("singleton")
+         * 같은 인스턴스를 조회한다.
+         * SingletonBean는 init하고 destory한다.
+         * 초기화 메서드 실행시점 : 스프링 컨테이너 생성 시점
+         * 스프링컨테이너가 관리하기에 종료 빈 메서드 실행함(destroy)
+         */
+    }
+
+    @Scope("singleton")
+    static class SingletonBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
