b562c217cd4e622d487cb91556248b95e7f627a4
slrslrr2 <slrslrr1@gmail.com>
2022-05-22 11:21:14 +0900

    스코프와 Provider 	- request

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,37 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+    private final LogDemoService logDemoService;
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+    /**
+     생성되는 시점을 싱글톤으로 올려서 MyLogger객체가 생성되는 것이 아니라 (이렇게하면 Scope=request라서 request요청이 없기에 오류남)
+     ==> ObjectProvider를 사용해서 생성시점을 getObject()할 때로 변경함으로써 아래 오류를 막아준다.
+        Error creating bean with name 'myLogger': Scope 'request' is not active for the current thread;
+     */
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request){
+        MyLogger myLogger = myLoggerProvider.getObject();
+
+        String requetURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requetURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+
+        return "OK";
+    }
+}
