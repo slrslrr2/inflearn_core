2224a86b926a1537b42eb8114e6f636dd631f57a
slrslrr2 <slrslrr1@gmail.com>
2022-05-23 21:14:45 +0900

    스코프(request)

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,9 +9,10 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) // CGLIB라는 라이브러리로 내 클래스를 상속 받은 가짜 프록시 객체를 만들어서 주입한다.
+                                                                    // 이 가짜 프록시 객체는 실제 요청이 오면 그때 내부에서 실제 빈을 요청하는 위임 로직이 들어있다.
+                                                                    // ==> Provider을 사용하는것보다 proxyMode를 사용하면 싱글톤처럼 사용할수있고, 클라이언트 소스 코드도 수정할 필요 없다.
 public class MyLogger {
-
     private String uuid;
     private String requestURL;
 
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -14,18 +14,20 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
     private final LogDemoService logDemoService;
 //    private final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
     /**
+     * private final ObjectProvider<MyLogger> myLoggerProvider;
      생성되는 시점을 싱글톤으로 올려서 MyLogger객체가 생성되는 것이 아니라 (이렇게하면 Scope=request라서 request요청이 없기에 오류남)
      ==> ObjectProvider를 사용해서 생성시점을 getObject()할 때로 변경함으로써 아래 오류를 막아준다.
         Error creating bean with name 'myLogger': Scope 'request' is not active for the current thread;
      */
 
+
+    private final MyLogger myLogger;
+
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request){
-        MyLogger myLogger = myLoggerProvider.getObject();
-
         String requetURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requetURL);
 
